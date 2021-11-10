332c21e911527d29b14e7d6f749f9c80bb82cdd7
Nick <nick.zimanski@gmail.com>
2021-11-09 19:20:51 -0500

    beginning of js code

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-{
-    
-}
+(function() {
+    Object.defineProperty(Array.prototype, byIndex, (index) => {
+        let midPoint = this.length / 2;
+        if (this[midPoint].index < index) {
+            return this.byIndex(this.slice(0, midPoint));
+        }
+        else if (this[midPoint].index > index) {
+            return this.byIndex(this.slice(midPoint + 1, this.length));
+        }
+        else return this[midPoint];
+    }); 
+})();
